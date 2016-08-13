2bef0c8a88d85c53fb61f2e2f9254fe160b8cf9c
sinsky <wesky93@gmail.com>
2016-08-13 21:09:00 +0900

    반대 손모양 알려주는 함수 추가

--- player.py
+++ player.py
@@ -1,6 +1,13 @@
 from random import choice
 
-
+def 반대손모양(손모양):
+    """반대다 손모양을 알려준"""
+    if 손모양 == 'gawi':
+        return 'bawi'
+    elif 손모양 == 'bawi':
+        return 'bo'
+    else:
+        return 'gawi'
 
 def show_me_the_hand(records):
     # 100번 까지는 무작위
